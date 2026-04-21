eff41e58bd610764b50abe8f5813b3ecddcaccff
Santiago Moreno <Santiago.moreno@usantoto.edu.co>
2026-04-20 20:17:41 -0500

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,32 @@
+require('dotenv').config()
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const mongoose = require('mongoose')
 const app = express()
+const Person = require('./models/person')
 
-morgan.token('body', (req) => {
-  return req.method === 'POST' ? JSON.stringify(req.body) : ''
-})
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json()) 
 app.use(express.static("dist"))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 let currentDate = new Date()
 
-const generateId = () => {
-  const randomId = Math.floor(Math.random() * 1000)
-  return randomId
-}
-
 app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
     res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -54,51 +37,41 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
-      return res.json(person) 
-    } else {
-      res.status(404).send("Put a valid ID to find the person's info you're looking for")
-    }
+    Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => res.status(400).send({ error: 'malformatted id'}))
 })
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
-    const checkRepeated = persons.some(p => p.name.toLowerCase() === body.name.toLowerCase()) 
-
-    if (!body.name) {
-      return res.status(404).json({
-        error: "name is missing"
-      })
-    } else if (!body.number) {
-      return res.status(404).json({
-        error: "number is missing"
-      })
-    } else if (checkRepeated) {
-      return res.status(404).json({
-        error: "name must be unique"
+    if (!body.name || !body.number) {
+      return res.status(400).json({
+        error: 'name or number missing'
       })
     }
 
-    const person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number,
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
 
-    persons = persons.concat(person) 
-    res.json(person)
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.statusMessage = "Key: " + id + " deleted successfully "
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
 })
 
 const unknownEndpoint = (req, res) => {
